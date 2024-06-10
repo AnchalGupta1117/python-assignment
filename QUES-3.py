435df8f94e827387cadf405e2187f29f64fc2292
AnchalGupta1117 <mehakanchal1068@gmail.com>
2024-06-10 14:11:53 +0530

    add files

---
+++ QUES-3.py
@@ -0,0 +1,8 @@
+number = int(input("enter a number: "))
+def factorial(num):
+    if (num == 0 or num == 1): 
+        return 1
+    else: 
+        return   num*factorial(num-1)
+result = factorial(number)
+print(f"the factorial of {number} is {result},")
